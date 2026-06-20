cdcd1def9d5d865186ee5922afe808f2996516fe
sidoncode <siddharth54330@gmail.com>
2026-06-20 10:27:50 +0530

    Customize for my AWS account

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -2,10 +2,10 @@ terraform {
   required_version = ">= 1.5.0"
 
   backend "s3" {
-    bucket         = "sid07devops-accelerator-platform-tf-state"
+    bucket         = "devops-capproject-sid112-tf-state"
     key            = "global/devops-accelerator/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "devops-accelerator-tf-locker"
+    dynamodb_table = "devops-capproject-sid113-tf-locker"
     encrypt        = true
   }
 }
